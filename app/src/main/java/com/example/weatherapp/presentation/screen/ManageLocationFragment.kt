42f04a25adf089261bd9d517953ccf66d2856b77
Morshu03 <varvaroom03@gmail.com>
2023-04-21 01:04:52 +0300

    added fragment_manage_location.xml

--- app/src/main/java/com/example/weatherapp/presentation/screen/ManageLocationFragment.kt
+++ app/src/main/java/com/example/weatherapp/presentation/screen/ManageLocationFragment.kt
@@ -1,4 +1,4 @@
-package com.example.weatherapp.presentation.screen.weather
+package com.example.weatherapp.presentation.screen
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,27 +7,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.weatherapp.R
+import com.example.weatherapp.databinding.FragmentManageLocationBinding
 import com.example.weatherapp.databinding.FragmentWeatherBinding
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
-class WeatherFragment : Fragment() {
-    private var _binding: FragmentWeatherBinding? = null
+class ManageLocationFragment : Fragment() {
+    private var _binding: FragmentManageLocationBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentWeatherBinding.inflate(inflater, container, false)
+        _binding = FragmentManageLocationBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.addNewCityButton.setOnClickListener {
-            findNavController().navigate(R.id.action_weatherFragment_to_manageLocationFragment)
+        binding.arrowBack.setOnClickListener {
+            findNavController().navigate(R.id.action_manageLocationFragment_to_weatherFragment)
         }
     }
-
 }
